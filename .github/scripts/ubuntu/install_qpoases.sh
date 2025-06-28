d8d7355950733f76f1fb355231ad158ac29c8ce2
Juan José Quiroz Omaña <juanjqogm@gmail.com>
2025-06-28 18:54:42 +0100

    Update install_qpoases.sh

---
+++ .github/scripts/ubuntu/install_qpoases.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ~/Downloads
+git clone https://github.com/coin-or/qpOASES.git
+cd qpOASES
+sed -i -e 's/option(BUILD_SHARED_LIBS "If ON, build shared library instead of static" OFF)/option(BUILD_SHARED_LIBS "If ON, build shared library instead of static" ON)/g' CMakeLists.txt
+mkdir build
+cd build
+cmake ..
+make 
+sudo make install
+
+
