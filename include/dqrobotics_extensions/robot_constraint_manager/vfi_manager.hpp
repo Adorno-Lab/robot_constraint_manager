28dfe734a92b99739f86f3ec16d2dc7cb2296a5e
Juancho <juanjqogm@gmail.com>
2025-10-21 15:10:07 +0100

    updated the copyright

--- include/dqrobotics_extensions/robot_constraint_manager/vfi_manager.hpp
+++ include/dqrobotics_extensions/robot_constraint_manager/vfi_manager.hpp
@@ -1,5 +1,5 @@
 /*
-#    Copyright (c) 2024 Adorno-Lab
+#    Copyright (c) 2024-2025 Adorno-Lab
 #
 #    robot_constraint_manager is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Lesser General Public License as published by
